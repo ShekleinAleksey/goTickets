7311e23bb0212ffd3b5430506c98cc6f14a00c3e
Sheklein Aleksey <sheklein.al@yandex.ru>
2025-03-02 17:58:56 +0300

    1

--- internal/handler/movie_screening.go
+++ internal/handler/movie_screening.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/ShekleinAleksey/goTickets/internal/entity"
 	"github.com/gin-gonic/gin"
@@ -90,4 +91,75 @@ func (h *Handler) GetMovieScreening(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, screening)
+}
+
+// @Summary Filter Movie Screenings
+// @Tags movie_screening
+// @Description get movie screenings filtered by movie ID and date
+// @ID filter-movie-screenings
+// @Produce json
+// @Param movie_id query int false "Movie ID"
+// @Param date query string false "Date (YYYY-MM-DD)"
+// @Success 200 {object} []entity.MovieScreening
+// @Failure 400 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /screenings/filter [get]
+func (h *Handler) FilterMovieScreenings(c *gin.Context) {
+	// Получаем параметры фильтрации
+	movieIDStr := c.Query("movie_id")
+	dateStr := c.Query("date")
+
+	// Получаем все сеансы
+	screenings, err := h.service.MovieScreeningService.GetAllMovieScreenings()
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Фильтруем по ID фильма, если указан
+	if movieIDStr != "" {
+		movieID, err := strconv.Atoi(movieIDStr)
+		if err != nil {
+			newErrorResponse(c, http.StatusBadRequest, "Invalid movie ID")
+			return
+		}
+
+		filteredScreenings := make([]entity.MovieScreening, 0)
+		for _, screening := range screenings {
+			if screening.MovieID == movieID {
+				filteredScreenings = append(filteredScreenings, screening)
+			}
+		}
+		screenings = filteredScreenings
+	}
+
+	// Фильтруем по дате, если указана
+	if dateStr != "" {
+		date, err := time.Parse("2006-01-02", dateStr)
+		if err != nil {
+			newErrorResponse(c, http.StatusBadRequest, "Invalid date format. Use YYYY-MM-DD")
+			return
+		}
+
+		filteredScreenings := make([]entity.MovieScreening, 0)
+		for _, screening := range screenings {
+			if screening.ShowTime.Format("2006-01-02") == date.Format("2006-01-02") {
+				filteredScreenings = append(filteredScreenings, screening)
+			}
+		}
+		screenings = filteredScreenings
+	}
+
+	// Загружаем информацию о фильмах для каждого сеанса
+	for i, screening := range screenings {
+		movie, err := h.service.MovieService.GetMovieByID(screening.MovieID)
+		if err != nil {
+			newErrorResponse(c, http.StatusInternalServerError, err.Error())
+			return
+		}
+		screenings[i].Movie = movie
+	}
+
+	c.JSON(http.StatusOK, screenings)
 }
